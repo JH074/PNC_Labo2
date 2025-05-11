d45f057c5375c7f4f37eb4a62240f032d712a4cf
JH074 <00162622@uca.edu.sv>
2025-05-11 17:19:45 -0600

    feat: inicio proyecto y conexion a postgressql

---
+++ PNC_LAB2/src/main/java/org/ncapas/pnc_lab2/PncLab2Application.java
@@ -0,0 +1,13 @@
+package org.ncapas.pnc_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PncLab2Application.class, args);
+    }
+
+}
---
+++ PNC_LAB2/src/test/java/org/ncapas/pnc_lab2/PncLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ncapas.pnc_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
