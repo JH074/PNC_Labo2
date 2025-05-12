cb450b39f71d180029a22d95ffd7bc7e8b6a36f3
JH074 <00162622@uca.edu.sv>
2025-05-11 20:43:08 -0600

    feat: entidad Departamento

--- PNC_LAB2/src/main/java/org/ncapas/pnc_lab2/Domain/Entities/Departamento.java
+++ PNC_LAB2/src/main/java/org/ncapas/pnc_lab2/Domain/Entities/Departamento.java
@@ -1,4 +1,24 @@
 package org.ncapas.pnc_lab2.Domain.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID idDepartamento;
+
+    private String codigo;
+
+    private String nombre;
 }
