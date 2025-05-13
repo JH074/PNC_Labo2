f305ee9fc11ebe4071f86c740018c01499518507
JH074 <00162622@uca.edu.sv>
2025-05-12 22:25:40 -0600

    feat: repositorio piso

--- PNC_LAB2/src/main/java/org/ncapas/pnc_lab2/Domain/Repositories/iCoordinacionRepository.java
+++ PNC_LAB2/src/main/java/org/ncapas/pnc_lab2/Domain/Repositories/iCoordinacionRepository.java
@@ -1,6 +1,5 @@
 package org.ncapas.pnc_lab2.Domain.Repositories;
 
-import org.ncapas.pnc_lab2.Domain.Entities.ColorBoton;
 import org.ncapas.pnc_lab2.Domain.Entities.Coordinacion;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
